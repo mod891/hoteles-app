6aceb89e36ab8590bf498e9b4b27c83122211c4d
David <skuayer91@gmail.com>
2024-02-11 01:26:45 +0100

    pruebas despliegue

--- database/migrations/2024_02_04_011806_create_rooms_table.php
+++ database/migrations/2024_02_04_011806_create_rooms_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('rooms', function (Bzzlueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignId('hotel_id');
